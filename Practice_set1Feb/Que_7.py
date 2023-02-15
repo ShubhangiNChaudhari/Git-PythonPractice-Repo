b7d275a56d2880a05ab419e49a557140447d655c
Shubhangi Chudhari <120164495+shubhangi699@users.noreply.github.com>
2023-02-15 13:29:58 -0800

    Today's Work 14 Feb 2023

---
+++ Practice_set1Feb/Que_7.py
@@ -0,0 +1,8 @@
+# Program to Find Even Numbers From a List
+
+for i in [1,56,89,88,21,58]:
+   if i%2==0:
+     print(i,'Number is Even')
+   else:
+       print(i,'Number is Odd')
+
---
+++ Practice_set1Feb/Que_9.py
@@ -0,0 +1,5 @@
+# Python program to split and join string
+list1 = 'wireless headphone paradies zebronics'
+
+print(list1.split(' '))
+print('-'.join(list1))
